16ef80ac361cde87289bb1b39b8e6e75ed3d297b
Артем Бухряков <artembuhryakov332@gmail.com>
2025-10-06 00:12:45 +0500

    Task5

--- Tyuiu.BukhryakovAY.Sprint2.Task5.V13.Lib/DataService.cs
+++ Tyuiu.BukhryakovAY.Sprint2.Task5.V13.Lib/DataService.cs
@@ -5,17 +5,17 @@ namespace Tyuiu.BukhryakovAY.Sprint2.Task5.V13.Lib
     {
         public string FindDateOfNextDay(int g, int m, int n)
         {
-            if (g >= 1 && g < 30 && m >= 1 && m <= 12)
+            if (n >= 1 && n < 30 && m >= 1 && m <= 12)
             {
-                g++;
-                string[] date = [g.ToString("00"), m.ToString("00"), n.ToString()];
+                n++;
+                string[] date = [n.ToString("00"), m.ToString("00"), g.ToString()];
                 return string.Join(".", date);
             }
             else if(g == 30 || g == 31)
             {
-                g = 1;
+                n = 1;
                 m++;
-                string[] date = [g.ToString("00"), m.ToString("00"), n.ToString()];
+                string[] date = [n.ToString("00"), m.ToString("00"), g.ToString()];
                 return string.Join(".", date);
             }
             else
--- Tyuiu.BukhryakovAY.Sprint2.Task5.V13/Program.cs
+++ Tyuiu.BukhryakovAY.Sprint2.Task5.V13/Program.cs
@@ -8,11 +8,11 @@ internal class Program
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("Введите год: ");
-        int n = Convert.ToInt32(Console.ReadLine());
+        int g = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите месяц: ");
         int m = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Введите день: ");
-        int g = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
