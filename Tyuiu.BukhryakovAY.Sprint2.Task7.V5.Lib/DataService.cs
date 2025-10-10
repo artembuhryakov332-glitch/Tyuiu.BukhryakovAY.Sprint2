c58e37d6f0340e09d858bab470250cfd3d2f5de4
Артем Бухряков <artembuhryakov332@gmail.com>
2025-10-10 21:32:03 +0500

    Task7

--- Tyuiu.BukhryakovAY.Sprint2.Task7.V5.Lib/DataService.cs
+++ Tyuiu.BukhryakovAY.Sprint2.Task7.V5.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.BukhryakovAY.Sprint2.Task7.V5.Lib
     {
         public bool CheckDotInShadedArea(double x, double y)
         {
-            if (Math.Pow(Math.E,x) <= y && Math.Pow(Math.E, -x) <= y && Math.Pow(x,2) <=y)
+            if ((Math.Pow(Math.E,x) >= y && Math.Pow(Math.E, -x) <= y && Math.Pow(x,2) <= y) || (x >= 0 && (y >= 0 && y<= 1) ))
             {
                 if ( (x >= 0 && x<= 1) && (x <= 0 && x >= -1) && (y >= 0 && y <= 1))
                 {
